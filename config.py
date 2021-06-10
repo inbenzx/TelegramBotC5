f37e0dd74f4ba92fe9818ae461bc24592ea5734c
inbenzx <83666309+inbenzx@users.noreply.github.com>
2021-06-10 17:17:25 +0300

    Adding bot's python files

---
+++ config.py
@@ -0,0 +1,12 @@
+
+TOKEN = "1817454191:AAEClEQCk6uXWGOURFti8by4WUolq4VMy8M"
+
+keys = {
+    'евро': 'EUR',
+    'доллар': 'USD',
+    'рубль': 'RUB',
+}
+
+API_KEY = "cdf157f930722832b547bed26a1961ff"
+
+
